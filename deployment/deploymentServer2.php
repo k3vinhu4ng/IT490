634e3c7de8c57f0102bc061344509957cdfa7f50
crisworks <cmb58@njit.edu>
2022-04-13 12:36:43 -0400

    added testing rollback not done yet doe

---
+++ deployment/deploymentServer2.php
@@ -0,0 +1,43 @@
+#!/usr/bin/php
+<?php
+
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('functions.php.inc');
+
+function packages($type, $package, $version){
+	$test = new testdb();
+	return $test->packages($type, $package, $version);
+}
+
+function rollback($badver){
+	$test = new testdb();
+	return $test->rollback($badver);
+}
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+  case "zip":
+      return packages($request['type'],$request['package'],$request['version']);
+  case "rollback":
+	return rollback($request['badver']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("deployment.ini","testServer");
+
+echo "Deployment Server BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "Deployment Server END".PHP_EOL;
+exit();
+?>
