df6ab7e9b6df11cce2e06f814e22683d0332a93a
crisworks <cmb58@njit.edu>
2022-04-13 13:42:49 -0400

    sorry....

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#send to deployment server
-sshpass -p 'IT490pass' scp /home/winonapatrick/winonapatrick/backups/* cristina@25.81.36.24:/var/www/sample
+
+sshpass -p 'IT490pass' scp /home/winonapatrick/winonapatrick/testing/* cristina@25.81.36.24:/var/www/sample
 
 
-#rm local copy 
-rm -r /home/winonapatrick/winonapatrick/backups/*
+rm -r /home/winonapatrick/winonapatrick/testing/*
