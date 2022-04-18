4a07d70aa5dc9df345e986df063a9f517bfce924
crisworks <cmb58@njit.edu>
2022-04-18 14:51:23 -0400

    testing db listener...

--- deployment/expkg.sh
+++ deployment/expkg.sh
@@ -3,4 +3,4 @@
 # extract package in a temp dir, put files into a temp files dir, put files to be seen on frontend
 sshpass -p 'it490' scp /home/cristina/testtar/* kevin@25.81.19.5:/home/kevin/testtar
 
-sshpass -p 'it490' ssh kevin@25.81.19.5 'cd ~/temp; cp ~/testtar/\'$1' ./; tar -xf \'$1'; cd ~/tempfiles; cp ~/temp/*/* ./; cd /var/www/sample; cp ~/tempfiles/* ./'
+sshpass -p 'it490' ssh kevin@25.81.19.5 'cd ~/temp; cp ~/testtar/\'$1' ./; tar -xf \'$1'; cd ~/tempfiles; cp ~/temp/*/* ./; cd /var/www/sample; cp ~/tempfiles/* ./; ./testRabbitMQServer.php'
