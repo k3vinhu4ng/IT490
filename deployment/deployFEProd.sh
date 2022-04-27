75d918a3d803ee90695f5b76fc0d59e902ac8c39
crisworks <cmb58@njit.edu>
2022-04-27 00:51:41 -0400

    updated scripts for correct layers

---
+++ deployment/deployFEProd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# extract package in a temp dir, put files into a temp files dir, put files to be seen on frontend
+sshpass -p 'password490' scp /home/cristina/testtar/* kevin@25.81.19.5:/home/kevin/testtar
+
+sshpass -p 'password490' ssh kevin@25.81.19.5 'cd ~/temp; cp ~/testtar/\'$1' ./; tar -xf \'$1'; cd ~/tempfiles; cp ~/temp/*/* ./; cd /var/www/sample; cp ~/tempfiles/* ./; echo 'password' | sudo -S systemctl restart apache2'
--- deployment/rollbackDBQA.sh
+++ deployment/rollbackDBQA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#this line removes the package
-sshpass -p 'password490' ssh kevin@25.81.19.5 'cd ~/temp; cp ~/testtar/\'$1' ./; tar -xf \'$1'; cd ~/tempfiles; cp ~/temp/*/* ./; cd /var/www/sample; cp ~/tempfiles/* ./'
+# need to change service so that the path is this one
+sshpass -p 'password490' ssh kevin@25.81.19.5 'cd ~/temp; cp ~/testtar/\'$1' ./; tar -xf \'$1'; cd ~/tempfiles; cp ~/temp/*/* ./; echo 'password' | sudo -S systemctl restart db.service'
 
 #need to remove the files that came from bad package 
 
